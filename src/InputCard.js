b25f2e97baf417af62e7788b26e6cb5bde5eb5be
Alexey Solovyev <solovyev.alexey@gmail.com>
2020-10-22 22:58:54 -0700

    Added examples.js

--- src/InputCard.js
+++ src/InputCard.js
@@ -5,6 +5,8 @@ import Form from 'react-bootstrap/Form';
 class InputCard extends React.Component {
   render() {
     const examplesId = this.props.id + '-examples';
+    const options = (this.props.examples || []).map(({name}, i) =>
+      <option key={i} value={name}>{name}</option>);
     return (
       <Card className={this.props.className} style={this.props.style}>
         <Card.Header className="py-1 pr-1">
@@ -12,8 +14,8 @@ class InputCard extends React.Component {
             <span>{this.props.title}</span>
             <Form.Label htmlFor={examplesId} className="ml-auto mr-2">Examples</Form.Label>
             <Form.Control id={examplesId} as="select" size="sm" custom>
-              <option value="kepler">kepler</option>
-              <option value="doppler">doppler</option>
+              <option value="--">--</option>
+              {options}
             </Form.Control>
           </Form>
         </Card.Header>
--- src/InputPane.js
+++ src/InputPane.js
@@ -3,12 +3,16 @@ import Button from 'react-bootstrap/Button';
 
 import InputCard from './InputCard';
 
+import {configExamples, inputExamples} from './examples';
+
 class InputPane extends React.Component {
   render() {
     return (
       <div className="d-flex flex-column h-100">
-        <InputCard id="input" className="mb-1" style={{flex: 3}} title="Input"/>
-        <InputCard id="config" className="my-1" style={{flex: 2}} title="Configuration"/>
+        <InputCard id="input" examples={inputExamples}
+          className="mb-1" style={{flex: 3}} title="Input"/>
+        <InputCard id="config" examples={configExamples} 
+          className="my-1" style={{flex: 2}} title="Configuration"/>
         <div>
           <Button variant="success" className="w-25">Run</Button>
           <Button variant="primary" className="ml-1 w-25">Clear</Button>
