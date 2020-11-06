04cc4405230996058b2b426e16df2c4f2641d6b8
Alexey Solovyev <solovyev.alexey@gmail.com>
2020-11-05 21:18:49 -0800

    ResultRow: plot relative and absolute errors for 1d problems

--- src/ResultRow.js
+++ src/ResultRow.js
@@ -28,42 +28,58 @@ function createData(foo, dom, samples = 500) {
   return data;
 }
 
-function addInfo(info, row, field, name) {
-  if (row[field]) {
-    const v = row[field];
-    const text = typeof v === 'string' ? v :
-                 typeof v === 'number' ? v.toString() :
-                 Array.isArray(v) ? `[${v.map(x => x.toString()).join(', ')}]` :
-                 v.toString();
-    info.push(<tr key={ field } className="bg-white"><td>{ name }</td><td>{ text }</td></tr>);
-  }
-  return info;
-}
+const infoParams = [
+  ['absErrorExactStr', 'Abs Error', 'absErrorModel'],
+  ['absErrorApproxStr', 'Abs Error (approx)'],
+  ['relErrorExactStr', 'Rel Error', 'relErrorModel'],
+  ['relErrorApproxStr', 'Rel Error (approx)'],
+  ['ulpErrorExactStr', 'ULP Error'],
+  ['ulpErrorApproxStr', 'ULP Error (approx)'],
+  ['realBounds', 'Bounds (without rounding)']
+];
 
 const ResultRow = ({row, update, data}) => {
-  const show = data && data.show;
-  const chartData = (data && data.data) || [];
-
-  const handleShow = () => {
-    const newData = { show: !show };
-    if (newData.show && row.absErrorModel && row.absErrorModel.expr) {
+  function handleShow(type, errModelField) {
+    const newData = {...data};
+    const show = newData[type + 'Show'] = !newData[type + 'Show'];
+    const chartData = newData[type + 'ChartData'];
+    if (show && row[errModelField] && row[errModelField].expr) {
       if (!chartData || !chartData.length) {
-        const f = eval(`(${row.absErrorModel.expr})`);
-        newData.data = createData(f, row.absErrorModel.dom, 500);
+        const f = eval(`(${row[errModelField].expr})`);
+        newData[type + 'ChartData'] = createData(f, row[errModelField].dom, 500);
       }
     }
     update(row, newData);
-  };
+  }
 
-  const info = [
-    ['absErrorExactStr', 'Abs Error'],
-    ['absErrorApproxStr', 'Abs Error (approx)'],
-    ['relErrorExactStr', 'Rel Error'],
-    ['relErrorApproxStr', 'Rel Error (approx)'],
-    ['ulpErrorExactStr', 'ULP Error'],
-    ['ulpErrorApproxStr', 'ULP Error (approx)'],
-    ['realBounds', 'Bounds (without rounding)']
-  ].reduce((info, [field, name]) => addInfo(info, row, field, name), []);
+  const info = [];
+
+  function addInfo(field, name, errModelField) {
+    if (row[field]) {
+      const v = row[field];
+      const text = typeof v === 'string' ? v :
+                   typeof v === 'number' ? v.toString() :
+                   Array.isArray(v) ? `[${v.map(x => x.toString()).join(', ')}]` :
+                   v.toString();
+      if (text) {
+        let plotButton = null;
+        if (errModelField && row[errModelField]) {
+          const type = errModelField.slice(0, 3);
+          const handle = handleShow.bind(null, type, errModelField);
+          const visible = (data || {})[type + 'Show'];
+          plotButton = <Button onClick={ handle } variant="success">{ visible ? "Hide" : "Plot" }</Button>;
+        }
+        info.push(
+          <tr key={ field } className="bg-white">
+            <td>{ name } { plotButton }</td>
+            <td>{ text }</td>
+          </tr>);
+      }
+    }
+    return info;
+  }
+
+  infoParams.forEach(args => addInfo(...args));
 
   return (
     <>
@@ -76,8 +92,8 @@ const ResultRow = ({row, update, data}) => {
         { row.absErrorModel.expr }
         { row.absErrorModel.dom.toString() }
       </pre> */}
-      <Button onClick={handleShow}>Plot</Button>
-      { show && <Chart data={ chartData }/> }
+      { data && data.absShow && <Chart title="Absolute Error" data={ data.absChartData }/> }
+      { data && data.relShow && <Chart title="Relative Error" data={ data.relChartData }/> }
     </>
   );
 }
