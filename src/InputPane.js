98c61e1de39eec9103c9db5e4b6c1dd94f93a71b
Alexey Solovyev <solovyev.alexey@gmail.com>
2020-11-07 22:58:33 -0800

    InputPane: converted into a functional component

--- src/InputPane.js
+++ src/InputPane.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import Button from 'react-bootstrap/Button';
 import Split from 'react-split';
 
@@ -9,67 +9,59 @@ import './cm-modes/fptaylor.js'
 
 import {configExamples, inputExamples} from './examples';
 
-class InputPane extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.inputRef = React.createRef();
-    this.configRef = React.createRef();
-  }
+// const createGutter = (index, dir) => {
+//   const gutter = document.createElement('div');
+//   gutter.className = `d-none d-md-block gutter gutter-${dir}`;
+//   return gutter;
+// }
 
-  onRunOrStop = () => {
-    this.props.onRunOrStop(this.inputRef.current.value, this.configRef.current.value);
-  }
+const InputPane = React.memo(({isRunning, onRunOrStop}) => {
+  const inputRef = useRef(null);
+  const configRef = useRef(null);
+  const handleRun = useCallback(() => 
+    onRunOrStop(inputRef.current.value, configRef.current.value));
+  
+  const runButton = isRunning ?
+      <Button variant="danger" className="w-25 py-0 h-100" onClick={handleRun}>Stop</Button> :
+      <Button variant="success" className="w-25 py-0 h-100" onClick={handleRun}>Run</Button>;
 
-  createGutter = (index, dir) => {
-    const gutter = document.createElement('div');
-    gutter.className = `d-none d-md-block gutter gutter-${dir}`;
-    return gutter;
-  }
-
-  render() {
-    const runButton = this.props.isRunning ?
-      <Button variant="danger" className="w-25 py-0 h-100" onClick={this.onRunOrStop}>Stop</Button> :
-      <Button variant="success" className="w-25 py-0 h-100" onClick={this.onRunOrStop}>Run</Button>;
-
-    return (
-      <div className="h-100">
-        <Split
-          sizes={[60, 40]}
-          minSize={100}
-          expandToMin={false}
-          gutterSize={6}
-          gutterAlign="center"
-          gutter={this.createGutter}
-          snapOffset={30}
-          dragInterval={1}
-          direction="vertical"
-          cursor="row-resize"
-          style={{height: 'calc(100% - 2.4rem)'}}
-        >
-        {/* <Split style={{height: 'calc(100% - 2.4rem)'}}> */}
-          <div className="pb-0" style={{height: '60%'}}>
-            <InputCard id="input" ref={this.inputRef}
-              className="h-100"
-              cmMode="fptaylor"
-              examples={inputExamples}
-              title="Input"/>
-          </div>
-          <div className="pb-2" style={{height: '40%'}}>
-            <InputCard id="config" ref={this.configRef}
-              className="h-100"
-              cmMode="fptaylor-config"
-              examples={configExamples} 
-              title="Configuration"/>
-          </div>
-        </Split>
-        <div style={{height: '2.4rem'}}>
-          {runButton}
-          {/* <Button variant="primary" className="ml-1 py-0 w-25 h-100" 
-            onClick={this.props.onClear}>Clear</Button> */}
+  return (
+    <div className="h-100">
+      <Split
+        sizes={[60, 40]}
+        minSize={100}
+        expandToMin={false}
+        gutterSize={6}
+        gutterAlign="center"
+        // gutter={createGutter}
+        snapOffset={30}
+        dragInterval={1}
+        direction="vertical"
+        cursor="row-resize"
+        style={{height: 'calc(100% - 2.4rem)'}}
+      >
+        <div className="pb-0" style={{height: '60%'}}>
+          <InputCard id="input" ref={inputRef}
+            className="h-100"
+            cmMode="fptaylor"
+            examples={inputExamples}
+            title="Input"/>
         </div>
+        <div className="pb-2" style={{height: '40%'}}>
+          <InputCard id="config" ref={configRef}
+            className="h-100"
+            cmMode="fptaylor-config"
+            examples={configExamples} 
+            title="Configuration"/>
+        </div>
+      </Split>
+      <div style={{height: '2.4rem'}}>
+        {runButton}
+        {/* <Button variant="primary" className="ml-1 py-0 w-25 h-100" 
+          onClick={this.props.onClear}>Clear</Button> */}
       </div>
-    );
-  }
-}
+    </div>
+  );
+});
 
 export default InputPane;
